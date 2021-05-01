ab01e6b46db07e1989ae2c5c497aaae2708c1133
unknown <amengede27@outlook.com>
2021-05-01 10:01:15 +1000

    vertex input

--- pyvulkan/shaders/compile.bat
+++ pyvulkan/shaders/compile.bat
@@ -1,2 +1,3 @@
 D:\dev\VulkanSDK\1.2.170.0\Bin32\glslc.exe shader.vert -o vert.spv
+D:\dev\VulkanSDK\1.2.170.0\Bin32\glslc.exe shader2.vert -o vert2.spv
 D:\dev\VulkanSDK\1.2.170.0\Bin32\glslc.exe shader.frag -o frag.spv
