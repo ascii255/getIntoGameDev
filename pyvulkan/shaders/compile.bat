94d6ab885cb8dc98ba67a268d879119f3ebde9d2
unknown <amengede27@outlook.com>
2021-05-04 17:12:17 +1000

    Improved lighting

--- pyvulkan/shaders/compile.bat
+++ pyvulkan/shaders/compile.bat
@@ -1,3 +1,4 @@
 D:\dev\VulkanSDK\1.2.170.0\Bin32\glslc.exe shader.vert -o vert.spv
 D:\dev\VulkanSDK\1.2.170.0\Bin32\glslc.exe shader2.vert -o vert2.spv
+D:\dev\VulkanSDK\1.2.170.0\Bin32\glslc.exe shader3.vert -o vert3.spv
 D:\dev\VulkanSDK\1.2.170.0\Bin32\glslc.exe shader.frag -o frag.spv
